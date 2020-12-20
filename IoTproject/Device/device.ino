b5bff7156d3e72a5b5c3fdeac7a37afab3b1ca62
oleksandra <oleksandrabovkun@godatadriven.com>
2020-12-20 23:04:17 +0100

    Me and my working setup

--- IoTproject/Device/device.ino
+++ IoTproject/Device/device.ino
@@ -1,13 +1,44 @@
 #include "AZ3166WiFi.h"
 #include "Sensor.h"
+#include "AzureIotHub.h"
+#include "DevKitMQTTClient.h"
+#include "RGB_LED.h"
+
+#define RGB_LED_BRIGHTNESS 32
 
 static DevI2C *ext_i2c;
 static HTS221Sensor *tempSensor;
+static LPS22HBSensor *pressureSensor;
+static LIS2MDLSensor *magneticSensor;
+static LSM6DSLSensor *accGyroSensor;
+
+static RGB_LED rgbLed;
+
 static float temperature;
 static float humidity;
-
+static float pressure;
 
 static boolean hasWifi = false;
+static boolean hasIoTHub = false;
+int counter = 0;
+
+const char *msg = "{\"temperature\": %f, \"humidity\": %f, \"pressure\": %f}";
+
+void blinkLED()
+{
+    rgbLed.turnOff();
+    rgbLed.setColor(0, 0, RGB_LED_BRIGHTNESS);
+    delay(500);
+    rgbLed.turnOff();
+}
+
+static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
+{
+  if (result == IOTHUB_CLIENT_CONFIRMATION_OK)
+  {
+    blinkLED();
+  }
+}
 
 static void initWifi()
 {
@@ -39,27 +70,88 @@ void setup() {
   hasWifi = false;
   initWifi();
   if (hasWifi == false) {return;}
-
+  
+  delay(5000); // because Azure needs some time 
+  if (!DevKitMQTTClient_Init()) 
+  {
+    hasIoTHub = false;
+    return;
+  }
+  hasIoTHub = true;
+  DevKitMQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
+  
   ext_i2c = new DevI2C(D14, D15);
-  Screen.print(3, "> Temperature sensor");
   tempSensor = new HTS221Sensor(*ext_i2c);
   tempSensor->init(NULL);
+
+  pressureSensor = new LPS22HBSensor(*ext_i2c);
+  pressureSensor->init(NULL);
+
+  magneticSensor = new LIS2MDLSensor(*ext_i2c);
+  magneticSensor->init(NULL);
+
+  accGyroSensor = new LSM6DSLSensor(*ext_i2c, D4, D5);
+  accGyroSensor->init(NULL);
+
+  accGyroSensor->enableAccelerator();
+  accGyroSensor->enableGyroscope();
+  accGyroSensor->enablePedometer();
+
+  accGyroSensor->setPedometerThreshold(LSM6DSL_PEDOMETER_THRESHOLD_MID_LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  char l[20];
+  counter = counter + 1;
 
-  if (hasWifi) {
+  if (digitalRead(USER_BUTTON_B)==LOW) {
+      Screen.clean();
+      Screen.print(0, "Go away!");
+      Screen.print(1, "You're ");
+      Screen.print(2, "interupting");
+      Screen.print(3, "me!!!");
+      do {
+          // do nothing 
+      }
+      while (digitalRead(USER_BUTTON_B)==LOW);
+      if (digitalRead(USER_BUTTON_B)==HIGH) {
+          Screen.clean();
+          Screen.print(0, "Thank you!");  
+      }
+    }
+  if (hasIoTHub && hasWifi && counter == 500) {
+    Screen.print(0, "Running...");
     tempSensor->enable();
     tempSensor->getTemperature(&temperature);
     Serial.printf("Temperature %f\n", temperature);
+    sprintf(l, "Temp.: %f", temperature); 
+    Screen.print(1, l);
 
     tempSensor->getHumidity(&humidity);
     Serial.printf("Humidity %f\n", humidity);
+    sprintf(l, "Humidity: %f", humidity); 
+    Screen.print(2, l);
+
+    pressureSensor->getPressure(&pressure);
+
+    char message[128];
+    snprintf(message, 128, msg, temperature, humidity, pressure);
+    
+    Screen.print(3, "Sending to IoT Hub...");
+    if (DevKitMQTTClient_SendEvent(message))
+    {
+      Screen.print(3, "Sent");
+    }
+    else
+    {
+      Screen.print(3, "Failure");
+    }
 
     tempSensor->disable();
     tempSensor->reset();
+    counter = 0;
   }
-  delay(5000);
+  delay(10);
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# IoT
+# Readme 
+
+## Installation
+1. Urls in the  `arduino.additionalUrls` settings for Tools are broken, that's why we updated them and specified path from this repo
+2. Install board with Board Manager
+3. For compiler `arm-none-eabi-gcc` it worked well, but for openocd not really. Openocd that we downloaded is the source code, not the binary. 
+3.1 To build Openocd on mac you need to install extra libraries with brew `brew install automake libtool libusb gcc@4.9 pkg-config`. Make sure you're in `~/Library/Arduino15/packages/AZ3166/tools/openocd/0.10.0`. Then you might need to change the Makefile to ignore warnings (for every warning open the Makefile and add it to GCC_WARNINGS -Wno-<name of error>), and then run `./configure --prefix=macosx && make && make install `. Then `cp -r macosx/share/openocd/scripts macosx` because VSCode Azure IoT extention expects different folder stucture. 
+4. Now you should be able to compile this project and push it to the device.
+
+## Code 
+Checks three sensors (temperature, humidity, and pressure) and sends it to Azure IoT. It also reacts on pressing button B. 
+
+
